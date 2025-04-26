c9b0b964b821da5e7b6d931496d72fdf4d261b31
Robert Böhm <me@rynti.com>
2025-04-26 21:40:45 +0200

    Apply VST changes from diedeno

--- src/appshell/view/appmenumodel.cpp
+++ src/appshell/view/appmenumodel.cpp
@@ -483,10 +483,16 @@ MenuItem* AppMenuModel::makeDiagnosticsMenu()
         makeMenuItem("diagnostic-show-profiler"),
     };
 
+    MenuItemList vstItems {
+        makeMenuItem("vst-use-oldview"),
+        makeMenuItem("vst-use-newview"),
+    }; 
+ 
     MenuItemList items {
         makeMenuItem("diagnostic-save-diagnostic-files"),
         makeMenuItem("playback-reload-cache"),
-        makeMenu(TranslatableString("appshell/menu/diagnostics", "&System"), systemItems, "menu-system")
+        makeMenu(TranslatableString("appshell/menu/diagnostics", "&System"), systemItems, "menu-system"),
+        makeMenu(TranslatableString("appshell/menu/diagnostics", "&VST"), vstItems, "menu-vst")      
     };
 
     if (isMuseSamplerModuleAdded()) {
